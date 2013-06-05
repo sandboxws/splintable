601e0e0b103912c016f1557271154229e1020726
Ahmed El.Hussaini <aelhussaini@gmail.com>
2013-06-06 00:37:37 +0200

    update to order priority

--- lib/splintable/generators/wordpress_dot_com_generator.rb
+++ lib/splintable/generators/wordpress_dot_com_generator.rb
@@ -12,15 +12,15 @@ module Splintable
       end
 
       def get_content
-        @content = @page.at('div.post')
-        @content = @page.at('div.entry') if @content.nil?
+        @content = @page.at('div.entry')
         @content = @page.at('div#single') if @content.nil?
-        @content = @page.at('div.post-wrapper') if @content.nil?
         @content = @page.at('.post-box') if @content.nil?
         @content = @page.at('div#content') if @content.nil?
         @content = @page.at('article') if @content.nil?
         @content = @page.at('.boxLeft') if @content.nil?
         @content = @page.at('div#main') if @content.nil?
+        @content = @page.at('div.post-wrapper') if @content.nil?
+        @content = @page.at('div.post') if @content.nil?
 
         @content.search('h1:first').remove
         @content.search('h2:first').remove
